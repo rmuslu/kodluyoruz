12ba39aebaa3b7052ec79814106e5130f6274a8d
rmuslu <49349809+rmuslu@users.noreply.github.com>
2021-05-23 13:52:18 +0300

    Add files via upload

---
+++ Create Server/index.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const server = http.createServer((req,res) => {
+
+    const url = req.url;
+    console.log(url);
+   
+  if(url === '/'){
+    res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h2>Index sayfasina hosgeldiniz</h2>")
+  }
+  else if(url === '/hakkimda'){
+    res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h2>Hakkimda sayfasina hosgeldiniz</h2>")
+  }
+  else if(url === '/iletisim'){
+    res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h2>İletisim sayfasına hosgeldiniz</h2>")
+  }
+  else{
+    res.writeHead(404, { "Content-Type": "text/html" });
+      res.write("<h2>404 SAYFA BULUNAMADI</h2>")
+  }
+    res.end();
+})
+
+const port = 5000;
+server.listen(port,() => {
+    console.log(`Sunucu port ${port} ile başlatıldı`);
+});
